4dc3ba167093d2cdb247245deefb8a311946d6d1
amal ziad <git config --global user.email>
2022-12-27 20:03:58 +0100

    Maj Compte rendu

--- gateway-service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Bean;
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 //    @Bean
@@ -25,16 +24,16 @@ public class GatewayServiceApplication {
 //                .route(r -> r.path("/products/**").uri("http://localhost:8082") )
 //                .build();
 //    }
-/*@Bean
-public RouteLocator routes(RouteLocatorBuilder builder){
-    return builder.routes()
-            .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE") )
-            .route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE") )
-            .build();
-}*/
-
+    /*@Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE") )
+                .route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE") )
+                .build();
+    }*/
     @Bean
-    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+                                                               DiscoveryLocatorProperties dlp){
         return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
     }
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,18 @@
 <h3> Compte rendu </h3>
-<h4> Customer & Inventory Services </h4>
-<img src="caputes/capture1.png">
-<h4> Billing Service </h4>
 <img src="caputes/capture2.png">
+<h5> 1.micro-service customer-service</h5>
+<img src="caputes/capture3.png">
+<h5> 2.micro-service inventory-service</h5>
+<img src="caputes/capture4.png">
+<h5> 3.la Gateway Spring cloud Gateway</h5>
+<img src="caputes/capture8.png">
+<h5> 4.Configuration statique du système de routage</h5>
+<img src="caputes/capture6.PNG">
+<h5> 5.l'annuaire Eureka Discrovery Service</h5>
+<img src="caputes/capture7.PNG">
+<h5> 6.configuration dynamique des routes de la gateway</h5>
+<img src="caputes/capture5.PNG">
+<h5> 7.le service de facturation Billing-Service </h5>
+<img src="caputes/capture9.PNG">
+
 
