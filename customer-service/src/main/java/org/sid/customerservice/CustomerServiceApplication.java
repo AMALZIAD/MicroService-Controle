d3c9041b6ddfbe7e30627fab72892afb1fa9c98a
amal ziad <git config --global user.email>
2023-01-09 13:40:09 +0100

    add mysql connection

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -5,6 +5,7 @@ import org.sid.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
@@ -17,11 +18,11 @@ public class CustomerServiceApplication {
     }
 
     @Bean
-    CommandLineRunner start(CustomerRepository customerRepository,
-                            RepositoryRestConfiguration restConfiguration  ){
+    CommandLineRunner start(CustomerRepository customerRepository ){
 
         return args -> {
-            restConfiguration.exposeIdsFor(Customer.class);
+            //restConfiguration.exposeIdsFor(Customer.class);,
+            //                            RepositoryRestConfiguration restConfiguration
             customerRepository.save(new Customer(null,"mouad","med@gmail.com"));
             customerRepository.save(new Customer(null,"salima","salima@gmail.com"));
             customerRepository.save(new Customer(null,"hind","hind@gmail.com"));
